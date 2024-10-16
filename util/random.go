f5b53b596486e11059f11b634c9a2b0931e38b7e
devswi <stayfocusjs@gmail.com>
2024-10-16 22:17:45 +0800

    feat: add unit test for account CRUD

---
+++ util/random.go
@@ -0,0 +1,46 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+var rng *rand.Rand
+
+func init() {
+	source := rand.NewSource(time.Now().UnixNano())
+	rng = rand.New(source)
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + rng.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rng.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "CAD"}
+	n := len(currencies)
+	return currencies[rng.Intn(n)]
+}
